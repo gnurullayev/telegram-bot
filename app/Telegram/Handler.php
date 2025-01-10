77fabe0636592e6eb2ab5ac22c57e067e27e6a8d
Gayrat Nurullayev <gnurullayev@corp.csolutions.lv>
2025-01-11 00:33:10 +0500

    changed

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -2,8 +2,8 @@
 
 namespace App\Telegram;
 
-use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Telegraph;
 
 class Handler extends WebhookHandler
 {
@@ -14,7 +14,7 @@ class Handler extends WebhookHandler
 
     public function start(): void
     {
-        // $this->re("salom botga hush kelibsiz");
-        Telegraph::video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")->send();
+        $this->reply("salom botga hush kelibsiz");
+        // Telegraph::video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")->send();
     }
 }
