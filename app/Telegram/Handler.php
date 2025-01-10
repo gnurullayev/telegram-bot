cbeb0c27ac276b7c3d38283c138721718b019240
Gayrat Nurullayev <gnurullayev@corp.csolutions.lv>
2025-01-11 01:28:57 +0500

    fasdf

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Telegram;
 
+use DefStudio\Telegraph\Facades\Telegraph as FacadesTelegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
-use DefStudio\Telegraph\Models\TelegraphChat;
+use DefStudio\Telegraph\Telegraph;
 
 class Handler extends WebhookHandler
 {
@@ -19,17 +20,16 @@ class Handler extends WebhookHandler
         // $this->reply(json_encode($this->bot->info()['id']));
         // $this->reply(json_encode($this->message->id()));
         $chat_id = $this->message->id();
-        $telegraph = TelegraphChat::find(1);
+        $telegraph = new Telegraph();
         // $updates = $telegraph->getUpdates();
         // $chat_id = $updates[0]->getMessage()->getChat()->getId;
         // Video yuborish
         try {
-            $this->reply("salom botga hush kelibsiz 1");
-            $telegraph->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")->send();
-            // $this->link(, "https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4");
-            // $bot->chat($chatId)
-            //     ->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")
-            //     ->send();
+            // $this->reply("salom botga hush kelibsiz 1");
+            // $telegraph->chat($chat_id)->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")->send();
+            FacadesTelegraph::chat($chat_id) // Telegram chat ID-ni kiriting
+                ->message("Ko'proq ma'lumot uchun <a href='https://topmovie.me'>Topmovie.me</a> saytiga tashrif buyuring!") // HTML formatdagi matn
+                ->send();
         } catch (\Exception $e) {
             $this->reply('Xato yuz berdi: ' . $e->getMessage());
         }
