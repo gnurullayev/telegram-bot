800ff7dc462c73079e20ec0ca797781f8b87b4a4
Gayrat Nurullayev <gnurullayev@corp.csolutions.lv>
2025-01-11 00:38:40 +0500

    changed

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -16,6 +16,12 @@ class Handler extends WebhookHandler
     {
         // $this->reply("salom botga hush kelibsiz");
         $telegraph = new Telegraph();
-        $telegraph->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")->send();
+        // Video yuborish
+        try {
+            $telegraph->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")
+                ->send();
+        } catch (\Exception $e) {
+            $this->reply('Xato yuz berdi: ' . $e->getMessage());
+        }
     }
 }
