7c338acfa517bf102239830ff7250e1d9fa3f3c6
Gayrat Nurullayev <gnurullayev@corp.csolutions.lv>
2025-01-11 01:03:10 +0500

    changed

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -16,17 +16,25 @@ class Handler extends WebhookHandler
     {
         // $this->bo("salom botga hush kelibsiz");
 
-        $this->reply(json_encode($this->bot->info()['id']));
+        // $this->reply(json_encode($this->bot->info()['id']));
         // $this->reply(json_encode($this->message->id()));
         $chatId = $this->message->id();
         $telegraph = new Telegraph();
         // $updates = $telegraph->getUpdates();
         // $chatId = $updates[0]->getMessage()->getChat()->getId;
         // Video yuborish
+        $bot = \DefStudio\Telegraph\Models\TelegraphBot::find(1);
         try {
-            $telegraph->chat($chatId)
-                ->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")
+            $bot->message("🎬 <b>Filming nomi:</b> Ajal poygasi")
+                ->video(storage_path('app/public/videos/ajal_poygasi.mp4')) // Videoning to'liq manzilini kiriting
+                ->caption("🎬 <b>Filming nomi:</b> Ajal poygasi\n\n/rand - 🎲 Random kinolar\n/top - 🏆 Top kinolar\n/last - ✨ Oxirgi yuklanganlar\n/help - 📞 Qo‘llab-quvvatlash\n/dev - 👨‍💻 Dasturchi")
+                ->keyboard([
+                    ['🎬 Ko‘proq Filmlar', '📥 Saqlab qo‘yish'], // Inline tugmalar
+                ])
                 ->send();
+            // $bot->chat($chatId)
+            //     ->video("https://topmovie.sgp1.cdn.digitaloceanspaces.com/Qizil-g'unchalar/G'unchalar%2010-qism%20480p%20O'zbek%20tilida.mp4")
+            //     ->send();
         } catch (\Exception $e) {
             $this->reply('Xato yuz berdi: ' . $e->getMessage());
         }
