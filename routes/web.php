7876f9d25fcbea4d18a33d6c46b9e6a6672033d7
gnurullayev <nurullayevgayrat73@gmail.com>
2025-02-03 01:43:01 +0500

    changed telegram

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@
 use App\Http\Controllers\TelegramController;
 use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
-
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-// Route::post('/telegram/webhook', function () {
-//     $updates = Telegram::commandsHandler(true);
+Route::post('/telegram/webhook', function () {
+    $updates = Telegram::commandsHandler(true);
 
-//     return 'OK';
-// });
+    return 'OK';
+});
 
-Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook'])->middleware(VerifyCsrfToken::class);
+// Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook'])->middleware(VerifyCsrfToken::class);
