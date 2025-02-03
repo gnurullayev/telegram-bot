f1a33f0e72dd2e8d934475faa3365362d5ee4c00
gnurullayev <nurullayevgayrat73@gmail.com>
2025-02-04 00:24:59 +0500

    added botUsers

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -53,7 +53,7 @@ class Handler extends WebhookHandler
 
         $message = "📌 *Bot foydalanuvchilari:*\n\n";
         foreach ($users as $user) {
-            $username = "" . $user->username;
+            // $username = "" . $user->username;
             $message .= "🆔 ID: {$user->telegram_id}\n";
             $message .= "👤 Ism: {$user->first_name}\n";
             // $message .= "📛 Username: @" . "alish" . "\n";
